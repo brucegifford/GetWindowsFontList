b2a2fa116563d02af66cf5fbb3f4672ad7ab086a
Bruce Gifford <b.gifford@comcast.net>
2023-07-14 11:53:33 -0600

    Get more attributes for each font
    make keys match what powershell is giving

--- GetWindowsFontList.py
+++ GetWindowsFontList.py
@@ -9,6 +9,67 @@ import win32com.client
 from win32com.shell import shell, shellcon
 import ctypes
 
+"""
+font_attribute_indexes = {
+    0: 'name',
+    1: 'path',
+    3: 'file_size',
+    4: 'file_type',
+    5: 'font_family',
+    6: 'font_style',
+    7: 'font_version',
+    8: 'unique_font_dentifier',
+    9: 'description',
+    10: 'copyright_notice',
+    11: 'trademark',
+    12: 'date_installed'
+}
+"""
+font_attribute_indexes = {
+    0: 'Title',
+    1: 'Font style',
+    2: 'Show/hide',
+    3: 'Designed for',
+    4: 'Category',
+    5: 'Designer/foundry',
+    6: 'Font Embeddability',
+    7: 'Font type',
+    8: 'Family',
+    9: 'Date created',
+    10: 'Date modified',
+    11: 'File size',
+    12: 'Collection',
+    13: 'Font file names',
+    14: 'Font version'
+}
+"""
+[
+0    'Modern Regular',
+1    'Regular',
+2    'Show',
+3    'Latin',
+4    'Text',
+5    'Microsoft Corporation',
+6    'Installable',
+7    'Raster',
+8    'Modern',
+9    '',
+10    '\u200e7/\u200e13/\u200e2023 \u200f\u200e8:52 PM',
+11    '8.50 KB',
+12    '',
+13    'C:\\WINDOWS\\Fonts\\MODERN.FON',
+14    '0.00', 
+'', '', '', '', '']
+"""
+
+def strip_LRM_chars(input_str):
+    if input_str:
+        output_str = input_str.encode('ascii', 'ignore')
+        output_str = output_str.decode()
+        return output_str
+    else:
+        return input_str
+
 
 def get_font_attributes():
     fonts = []
@@ -21,8 +82,24 @@ def get_font_attributes():
 
     for i in range(fonts_folder.Items().Count):
         font_item = fonts_folder.Items().Item(i)
+        font_attributes = {}
+        for index, key in font_attribute_indexes.items():
+            value = fonts_folder.GetDetailsOf(font_item, index)
+            if value == "":
+                value = None
+            font_attributes[key] = value
+        font_attributes['Date modified'] = strip_LRM_chars(font_attributes['Date modified'])
+        font_attributes['Date created'] = strip_LRM_chars(font_attributes['Date created'])
+        """
+        values = []
+        for j in range(30):
+            value = fonts_folder.GetDetailsOf(font_item, j)
+            values.append(value)
+        #print(values)
+        """
         font_name = fonts_folder.Items().Item(i).Name
         font_path = fonts_folder.Items().Item(i).Path
+        font_attributes['path'] = font_path
 
         #font_type = win32api.GetBinaryType(font_path)
         result = ctypes.c_ulong()
@@ -33,8 +110,13 @@ def get_font_attributes():
                 font_type = 'OpenType'
         else:
             font_type = 'Unknown'
+        font_attributes['font_type'] = font_type
 
-        font_attributes = {
+        if 'Font file names' in font_attributes and font_attributes['Font file names'] is not None:
+            font_attributes['Font file names'] = font_attributes['Font file names'].replace('\\','/')
+        if 'path' in font_attributes:
+            font_attributes['path'] = font_attributes['path'].replace('\\','/')
+        font_attributes_old = {
             'Name': font_name,
             'Path': font_path,
             'Type': 'TrueType' if font_type == win32con.SCS_32BIT_BINARY else 'OpenType',
@@ -42,7 +124,7 @@ def get_font_attributes():
         }
         fonts.append(font_attributes)
 
-    fonts.sort(key=lambda k: k["Name"])
+    fonts.sort(key=lambda k: k["Title"].lower())
 
     return fonts
 
@@ -74,7 +156,7 @@ def Main():
         if args.font_names:
             with open(args.font_names, "w", encoding='utf-8') as data_file:
                 for font in font_list:
-                    data_file.write(font["Name"]+'\n')
+                    data_file.write(font["Title"]+'\n')
 
 
 
